172612ad6b06475a8e303fcef8e68d4be4807e4a
Mehul Mandviya <mehul@Mehuls-MacBook-Pro.local>
2019-01-04 15:59:50 -0800

    Completed all the tests for the sprint.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,35 @@
 var Stack = function() {
   var someInstance = {};
+  someInstance.len = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[someInstance.len] = value;
+    someInstance.len++;
   };
 
-  someInstance.pop = function() {
+  someInstance.pop = function() { 
+    var lastVal = storage[someInstance.len-1];
+    delete storage[someInstance.len]; 
+    someInstance.len--;
+    if(someInstance.len < 0){
+      someInstance.len = 0;
+    }
+     return lastVal;
   };
 
   someInstance.size = function() {
-  };
-
+    return someInstance.len;
+  }
   return someInstance;
 };
+
+// delete storage[someInstance.len]; 
+ //someInstance.len--;
+//if(someInstance.len < 0){
+    //   someInstance.len = 0;
+    // }
+// return storage[someInstance.len-1];
