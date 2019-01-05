8802b8007f751b0e0f0ac878a1ebf66127fec7bf
Mehul Mandviya <mehul@Mehuls-MacBook-Pro.local>
2019-01-05 14:05:53 -0800

    Completed basic requirements for queue in prototypal.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.len = 0;
+  someInstance.storage = {};
+  
+  return someInstance;
+
 };
 
 var stackMethods = {};
+  
+  stackMethods.push = function(value){
+     this.storage[this.len] = value;
+     this.len++;
+  };
 
+  stackMethods.pop = function(){
+    var lastVal = this.storage[this.len-1];
+    delete this.storage[this.len]; 
+    this.len--;
+    if(this.len < 0){
+       this.len = 0;
+    }
+    return lastVal;
+  };
+
+  stackMethods.size = function(){
+    return this.len;
+  };
 
