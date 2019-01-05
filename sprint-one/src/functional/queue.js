c9874e53ecb65eecbde2aca248e9c209dee38326
Mehul Mandviya <mehul@Mehuls-MacBook-Pro.local>
2019-01-04 22:34:02 -0800

    Completed basic requirements of queue.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,30 @@
 var Queue = function() {
   var someInstance = {};
-
+  someInstance.len = 0;
+  someInstance.firstKey = 0;
   // Use an object with numeric keys to store values
   var storage = {};
-
+  
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+  storage[someInstance.len] = value;
+  someInstance.len++;
   };
 
   someInstance.dequeue = function() {
+    var firstEl = storage[someInstance.firstKey];
+    if(someInstance.len - someInstance.firstKey <= 0){
+      return 0;
+    } else {
+    delete(storage[someInstance.firstKey]);
+    someInstance.firstKey++; 
+    return firstEl;
+  }
   };
 
-  someInstance.size = function() {
+  someInstance.size = function() { 
+    return someInstance.len - someInstance.firstKey;
   };
 
   return someInstance;
