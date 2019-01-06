4ecc5ee5b8bc0f41d64c7d380df9136adfc76d80
Mehul Mandviya <mehul@Mehuls-MacBook-Pro.local>
2019-01-05 16:52:08 -0800

    Completed all the basic requirements.

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,28 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.len = 0;
+    this.firstKey = 0;
+    this.storage = {};
+  }
+  enqueue(value){
+    this.storage[this.len] = value;
+    this.len++;
+  }
+  dequeue(){
+  	var firstEl = this.storage[this.firstKey];
+ 	if(this.len - this.firstKey <= 0){
+ 		return 0;
+ 	} else {
+ 		delete(this.storage[this.firstKey]);
+        this.firstKey++; 
+        return firstEl;
+ 	}
+
+  }
+  size(){
+  	return this.len - this.firstKey;
   }
 
 }
+ var stackstor = new Queue();
