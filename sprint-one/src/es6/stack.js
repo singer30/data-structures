fa9406f65c3499f39cc7b19873285e0ec18515ac
Mehul Mandviya <mehul@Mehuls-MacBook-Pro.local>
2019-01-05 16:45:22 -0800

    Completed the basic requirements for stack in ES6 pattern.

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.len = 0;
+  	this.storage = {};
+  }
+  push(value){
+  	this.storage[this.len] = value;
+    this.len++;
+
+  }
+  pop(){
+  var lastVal = this.storage[this.len-1];
+    delete this.storage[this.len]; 
+    this.len--;
+    if(this.len < 0){
+       this.len = 0;
+    }
+    return lastVal;
+  }
+  size(){
+  	return this.len;
   }
 
 }
+
+ var stackstor = new Stack();
