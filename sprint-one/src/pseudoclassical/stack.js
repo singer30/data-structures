76328c5f67f7712137894194777e1fab0976fffc
Mehul Mandviya <mehul@Mehuls-MacBook-Pro.local>
2019-01-05 16:18:13 -0800

    Completed basic requirements for stack.js for pseudoclassical instantaiation.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.len = 0;
+  this.storage = {};
 };
+  Stack.prototype.push = function(value){
+     this.storage[this.len] = value;
+      this.len++;
+  };
 
+  Stack.prototype.pop = function(){
+   var lastVal = this.storage[this.len-1];
+    delete this.storage[this.len]; 
+    this.len--;
+    if(this.len < 0){
+       this.len = 0;
+    }
+    return lastVal;
+  };
+
+  Stack.prototype.size = function(){
+    return this.len;
+  };
+
+ var stackinfo = new Stack();
 
