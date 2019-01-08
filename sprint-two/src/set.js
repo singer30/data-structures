9bb951b7a4424fd7bffc62f235cb2822aa50a612
Mehul Mandviya <mehul@Mehuls-MacBook-Pro.local>
2019-01-08 08:45:30 -0800

    Completed the basic requirements for set.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,29 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+ if(!this._storage.includes(item)){
+ this._storage.push(item);
+ }
+ //return false;
 };
 
 setPrototype.contains = function(item) {
+if( this._storage.includes(item)){
+	return true;
+}
+   return false;
 };
 
 setPrototype.remove = function(item) {
+	if( this._storage.includes(item)){
+	this._storage.pop(item);
+}
 };
 
 /*
