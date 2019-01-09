01835bd7069e0f651a3a742101fa5e66807b4e74
singer30 <lukas.w.singer@gmail.com>
2019-01-08 16:09:59 -0800

    Completed the tests where we were successfully able to add children to array, check if the target is present in the object and return a boolean value.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -9,33 +9,33 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-	// create a tree that has the value from user
-	// we need to push the new tree in array
-	var treeChild = Tree(value);
-	this.children.push(treeChild);
+  // create a tree that has the value from user
+  // we need to push the new tree in array
+  var treeChild = Tree(value);
+  this.children.push(treeChild);
 };
 
 treeMethods.contains = function(target) {
-	// declare a result as false
-	// if targetnodes value = target  set result as true
-	// else check if targetnodes have children
-	// if yes loop through the children
-	// check if children value is equal to target then set the result as true
-	// return result
-	var result = false;
-	var checkTarget = function(obj){
-		var currentTree = obj;
-		if(currentTree.value === target){
-			result = true;
-		}if(currentTree.children){
-			for(var i = 0; i < currentTree.children.length;i++){
-				checkTarget(currentTree.children[i]);
-			}
-		}
+  // declare a result as false
+  // if targetnodes value = target  set result as true
+  // else check if targetnodes have children
+  // if yes loop through the children
+  // check if children value is equal to target then set the result as true
+  // return result
+  var result = false;
+  var checkTarget = function(obj) {
+    var currentTree = obj;
+    if (currentTree.value === target) {
+      result = true;
+    } if (currentTree.children) {
+      for (var i = 0; i < currentTree.children.length; i++) {
+        checkTarget(currentTree.children[i]);
+      }
+    }
 
-	}
-	checkTarget(this);
-    return result;
+  };
+  checkTarget(this);
+  return result;
 
 };
 
