7b96405b491d5ada144b0509e0c241cfaa0d235d
singer30 <lukas.w.singer@gmail.com>
2019-01-08 17:01:12 -0800

    Implementing the requirements of binarySearch tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,25 @@
 var BinarySearchTree = function(value) {
+  // var binaryTree = {};
+  // binaryTree.value = value;
+  // binaryTree.children = [];
+  // var leftnum = {};
+  // var rightnum = {};   
+  // _.extend(binaryTree, binaryTreeMethods);
+  // return binaryTree;
 };
 
+// var binaryTreeMethods = {};
+// /*
+//  * Complexity: What is the time complexity of the above functions?
+//  */
+// binaryTreeMethods.insert = function(value) {
+  
+// };
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+// binaryTreeMethods.contains = function() {
+  
+// };
+
+// binaryTreeMethods.depthFirstLog = function() {
+  
+// };
