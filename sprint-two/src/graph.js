fc9f79aee74a791ed2a903b837007dddb7e4ca25
singer30 <lukas.w.singer@gmail.com>
2019-01-08 16:56:52 -0800

    Implemented basic tests of graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,10 +1,12 @@
 
 
 // Instantiate a new graph
+// create a storage object
+// set a count as a  as 0.
 var Graph = function() {
+ 
   this.storage = {};
   this.count = 0;
-  
 };
 
 // Add a node to the graph, passing in the node's value.
@@ -15,7 +17,7 @@ Graph.prototype.addNode = function(node) {
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  if(this.storage[node]){
+  if (this.storage[node]) {
     return true;
   }
   return false;
@@ -23,14 +25,22 @@ Graph.prototype.contains = function(node) {
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+
   delete this.storage[node];
-  delete this.storage[node].removeEdge();
+  this.count--;
+  for (var key in this.storage) {
+    if (this.storage[key][node]) {
+      delete this.storage[key][node];
+    }
+  }
+  //delete this.storage[node].removeEdge();
+  
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
   var result = false;
-  if (this.storage[fromNode][toNode] || this.storage[toNode][fromNode]){
+  if (this.storage[fromNode][toNode] && this.storage[toNode][fromNode]) {
     result = true;
   }
   return result;
@@ -50,6 +60,11 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var key in this.storage) {
+    console.log(cb);
+    cb.call(this.storage[key], key);
+    console.log(this.storage[key]);
+  }  
 };
 
 /*
